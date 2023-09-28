e9d28f0650aa16c069ca039da49247fbfbbbd743
Akash-152000 <akash152000akash@gmail.com>
2023-09-28 17:28:42 +0530

    Created all the Property apis

--- controllers/propertyController.js
+++ controllers/propertyController.js
@@ -1,11 +1,7 @@
 const Property = require("../models/property");
 
-exports.getAllProperties = async (req, res, next) => {
+exports.getAllProperties = async (req, res) => {
   const properties = await Property.find();
-
-  if (!properties) {
-    return next(new Errhandler("Errrrrr", 500));
-  }
 
   res.status(200).json({
     success: true,
@@ -13,7 +9,7 @@ exports.getAllProperties = async (req, res, next) => {
   });
 };
 
-exports.addProperty = async (req, res, next) => {
+exports.addProperty = async (req, res) => {
   const { propertyName, propertyAddress, rooms, toilets, area } = req.body;
   const property = await Property.create({
     propertyName,
@@ -26,5 +22,59 @@ exports.addProperty = async (req, res, next) => {
   res.status(201).json({
     success: true,
     property,
+  });
+};
+
+exports.getProperty = async (req, res) => {
+  let property = await Property.findById(req.params.id);
+  if (!property) {
+    return res.status(404).json({
+      success: false,
+      message: "property not found",
+    });
+  }
+
+  return res.status(200).json({
+    success: true,
+    property,
+  });
+};
+
+exports.updateProperty = async (req, res) => {
+  let property = await Property.findById(req.params.id);
+
+  if (!property) {
+    return res.status(404).json({
+      success: false,
+      message: "property not found",
+    });
+  }
+
+  property = await Property.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+    useFindAndModify: false,
+  });
+
+  res.status(200).json({
+    success: true,
+    property,
+  });
+};
+
+exports.deleteProperty = async (req, res) => {
+  const property = await Property.findById(req.params.id);
+
+  if (!property) {
+    return res.status(404).json({
+      success: false,
+      message: "property not found",
+    });
+  }
+
+  property.deleteOne();
+
+  res.status(200).json({
+    success: true,
   });
 };
--- routes/propertyRoute.js
+++ routes/propertyRoute.js
@@ -1,8 +1,9 @@
 const express = require('express')
-const { getAllProperties, addProperty } = require('../controllers/propertyController')
+const { getAllProperties, addProperty, deleteProperty, updateProperty, getProperty } = require('../controllers/propertyController')
 const router = express.Router()
 
-router.route('/properties').get(getAllProperties)
-router.route('/owner/addproperty').post(addProperty)
+router.route('/list-properties').get(getAllProperties)
+router.route('/property').post(addProperty)
+router.route('/property/:id').delete(deleteProperty).put(updateProperty).get(getProperty)
 
 module.exports = router;
