16359183c77be76b999760fe18c9ee665506be36
Akash-152000 <akash152000akash@gmail.com>
2023-09-28 14:12:09 +0530

    added public folder

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 const express = require('express')
 const dotenv = require('dotenv')
+const path = require('path')
 const connectToMongo = require('./config/dbConnection')
 
 
 // Config
 dotenv.config()
 
+const app = express();
+
+// Middlewares
+app.use(express.static(path.resolve('./public')))
+
 // MongoDB Connection
 connectToMongo();
 
-const app = express();
+
+
+
 
 app.listen(process.env.PORT,()=>console.log(`Server Running on port: ${process.env.PORT}`))
