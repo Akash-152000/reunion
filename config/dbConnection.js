3df143683da124ba9a4ebf2ea78733aefebc7cd0
Akash-152000 <akash152000akash@gmail.com>
2024-01-29 18:06:53 +0530

    added try catch and async await to dbConnection.js

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,14 +1,18 @@
 const mongoose = require("mongoose");
 
-const connectToMongo = () => {
-  mongoose
-    .connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    })
-    .then((data) =>
-      console.log(`Mongo DB connected with server: ${data.connection.host}`)
-    );
+const connectToMongo = async () => {
+  try {
+    await mongoose
+      .connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      })
+      .then((data) =>
+        console.log(`Mongo DB connected with server: ${data.connection.host}`)
+      );
+  } catch (error) {
+    console.log("Error in connecting to DB", error)
+  }
 };
 
 module.exports = connectToMongo;
